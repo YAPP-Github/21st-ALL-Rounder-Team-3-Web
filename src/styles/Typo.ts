a390daae28a563d6b90afc8b9832ec6a0f102cc4
woogie <dmsdmr9411@gamil.com>
2022-12-04 12:13:04 +0900

    [feat] Typo system

---
+++ src/styles/Typo.ts
@@ -0,0 +1,91 @@
+import { css } from "styled-components";
+
+export const typo_h1_bold = css`
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 140%;
+`;
+
+export const typo_h1 = css`
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 140%;
+`;
+
+export const typo_h2 = css`
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 140%;
+`;
+
+export const typo_h3 = css`
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 140%;
+`;
+
+export const typo_h4 = css`
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 140%;
+`;
+
+export const typo_body1 = css`
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 140%;
+`;
+
+export const typo_body2 = css`
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 140%;
+`;
+
+export const typo_body3 = css`
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 140%;
+`;
+
+export const typo_cation1_semibold = css`
+  font-weight: 600;
+  font-size: 12px;
+  line-height: 140%;
+`;
+
+export const typo_cation1 = css`
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 140%;
+`;
+
+export const typo_cation2_semibold = css`
+  font-weight: 600;
+  font-size: 10px;
+  line-height: 140%;
+`;
+
+export const typo_cation2 = css`
+  font-weight: 400;
+  font-size: 10px;
+  line-height: 140%;
+`;
+
+export const typo_cation3 = css`
+  font-weight: 400;
+  font-size: 8px;
+  line-height: 140%;
+`;
+
+export const typo_button1 = css`
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 140%;
+`;
+
+export const typo_button2 = css`
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 140%;
+`;
