ca6ecbd5d09d8b85f1b8a95ef0e4c56c3a1d8249
Yejin Kim <qja086351@naver.com>
2022-12-21 12:17:41 +0900

    [design] 디바이더 컴포넌트 UI 구현

---
+++ src/components/common/ComponentDivider.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledComponentDivider = styled.div`
+  width: 100%;
+  background-color: #f4f4f4;
+  height: 8px;
+`;
+
+export const ComponentDivider = () => {
+  return <StyledComponentDivider />;
+};
---
+++ src/components/common/TextDivider.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledTextDivider = styled.div`
+  margin: 10px 16px;
+  background-color: #f4f4f4;
+  height: 1px;
+`;
+
+export const TextDivider = () => {
+  return <StyledTextDivider />;
+};
