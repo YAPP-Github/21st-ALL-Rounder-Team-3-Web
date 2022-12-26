f1542c9d3aecdc78a98e364ababb6366ca369c57
woogie <dmsdmr9411@gmail.com>
2022-12-26 22:57:33 +0900

    [feat] Typo 수정

--- src/styles/Typo.ts
+++ src/styles/Typo.ts
@@ -1,54 +1,67 @@
 import { css } from "styled-components";
 
+const typo_base = css`
+  font-family: "NotoSans";
+`;
+
 export const typo_h1_bold = css`
+  ${typo_base};
   font-weight: 700;
   font-size: 24px;
   line-height: 140%;
 `;
 
-export const typo_h1 = css`
+export const typo_h1_semibold = css`
+  ${typo_base};
   font-weight: 600;
   font-size: 24px;
   line-height: 140%;
 `;
 
-export const typo_h2 = css`
+export const typo_h2_semibold = css`
+  ${typo_base};
   font-weight: 600;
   font-size: 20px;
   line-height: 140%;
 `;
 
-export const typo_h3 = css`
+export const typo_h3_semibold = css`
+  ${typo_base};
   font-weight: 600;
   font-size: 16px;
   line-height: 140%;
 `;
 
-export const typo_h4 = css`
+export const typo_h4_semibold = css`
+  ${typo_base};
   font-weight: 600;
   font-size: 14px;
   line-height: 140%;
 `;
 
-export const typo_body1 = css`
+export const typo_body1_medium = css`
+  ${typo_base};
   font-weight: 500;
   font-size: 16px;
   line-height: 140%;
 `;
 
-export const typo_body2 = css`
+export const typo_body2_medium = css`
+  ${typo_base};
   font-weight: 500;
-  font-size: 14px;
+  font-size: 16px;
   line-height: 140%;
 `;
 
-export const typo_body3 = css`
+export const typo_body3_regular = css`
+  ${typo_base};
   font-weight: 400;
   font-size: 14px;
   line-height: 140%;
 `;
 
-export const typo_body4 = css`
+export const typo_body4_regular = css`
+  ${typo_base};
   font-weight: 400;
   font-size: 12px;
   line-height: 140%;
@@ -56,42 +69,49 @@ export const typo_body4 = css`
 `;
 
 export const typo_cation1_semibold = css`
+  ${typo_base};
   font-weight: 600;
   font-size: 12px;
   line-height: 140%;
 `;
 
-export const typo_cation1 = css`
+export const typo_cation1_regular = css`
+  ${typo_base};
   font-weight: 400;
   font-size: 12px;
   line-height: 140%;
 `;
 
 export const typo_cation2_semibold = css`
+  ${typo_base};
   font-weight: 600;
   font-size: 10px;
   line-height: 140%;
 `;
 
-export const typo_cation2 = css`
+export const typo_cation2_regular = css`
+  ${typo_base};
   font-weight: 400;
   font-size: 10px;
   line-height: 140%;
 `;
 
-export const typo_cation3 = css`
+export const typo_cation3_regular = css`
+  ${typo_base};
   font-weight: 400;
   font-size: 8px;
   line-height: 140%;
 `;
 
-export const typo_button1 = css`
+export const typo_button1_semibold = css`
+  ${typo_base};
   font-weight: 600;
   font-size: 16px;
   line-height: 140%;
 `;
 
-export const typo_button2 = css`
+export const typo_button2_medium = css`
+  ${typo_base};
   font-weight: 500;
   font-size: 14px;
   line-height: 140%;
