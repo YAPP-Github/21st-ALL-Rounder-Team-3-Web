05c53caa2e384e9a53e5694071df7972a33c5b71
woogie <dmsdmr9411@gmail.com>
2022-12-26 19:17:18 +0900

    [chore] 절대경로 옵션 수정

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src/"),
+      "@src": path.resolve(__dirname, "src"),
     },
     extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".json"],
   },
