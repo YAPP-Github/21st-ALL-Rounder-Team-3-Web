f9fd9ebd0029e300af730f31ef1232c8363da6e6
woogie <dmsdmr9411@gmail.com>
2022-12-26 22:56:38 +0900

    [style] GlobalStyle 경로 변경

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
-import GlobalStyle from "./globalStyles";
+import GlobalStyle from "./styles/globalstyle";
 
 const container = document.getElementById("root");
 const root = createRoot(container as Element);
