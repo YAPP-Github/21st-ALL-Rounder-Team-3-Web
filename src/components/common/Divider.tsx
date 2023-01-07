b71236bac5d0a769ede05b3745d8949e35d53087
Yejin Kim <qja086351@naver.com>
2023-01-08 04:07:34 +0900

    [refaactor] divider 컴포넌트 통일
    props로 타입을 받아 height를 지정, 이 외 다른 상황에서도 쓰일 수 있도록 함

--- src/components/common/Divider.tsx
+++ src/components/common/Divider.tsx
@@ -1,19 +1,45 @@
-import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+type Props = {
+  height?: number;
+  marginHorizontal?: number;
+  marginTop?: number;
+  marginBottom?: number;
+  dividerType?: "componentDivider" | "textDivider";
+};
+
+const Divider = ({ height, marginHorizontal, marginTop, marginBottom, dividerType }: Props) => {
+  return (
+    <StyledTextDivider
+      height={height}
+      marginHorizontal={marginHorizontal}
+      marginBottom={marginBottom}
+      marginTop={marginTop}
+      dividerType={dividerType}
+    />
+  );
+};
+
+const ComponentDivider = css`
+  height: 8px;
+`;
+const TextDivider = css`
+  height: 1px;
+  margin: 10px 16px;
+`;
 
 const StyledTextDivider = styled.div`
-  margin: 10px 0;
   background-color: #f4f4f4;
   height: ${(props: Props) => `${props.height}px` || "1px"};
   margin-left: ${(props: Props) => `${props.marginHorizontal}px` || "0px"};
   margin-right: ${(props: Props) => `${props.marginHorizontal}px` || "0px"};
+  margin-top: ${(props: Props) => `${props.marginTop}px` || "0px"};
+  margin-bottom: ${(props: Props) => `${props.marginBottom}px` || "0px"};
+
+  ${props => {
+    if (props.dividerType == "componentDivider") return ComponentDivider;
+    if (props.dividerType == "textDivider") return TextDivider;
+  }}
 `;
 
-type Props = {
-  height: number;
-  marginHorizontal?: number;
-};
-
-export const Divider = ({ height, marginHorizontal }: Props) => {
-  return <StyledTextDivider height={height} marginHorizontal={marginHorizontal} />;
-};
+export default Divider;
