c6ba1d8b0c9a5c8b7031f5ab47761c4b40f2cba1
Yejin Kim <qja086351@naver.com>
2023-01-07 16:35:35 +0900

    [chore] 파일명 변경

---
+++ src/components/task/TaskDescription.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+import { typo_body4_regular, typo_body2_medium } from "../../styles/Typo";
+
+const Wrapper = styled.div`
+  margin-left: 16px;
+  flex-direction: column;
+  display: flex;
+`;
+
+const Title = styled.div`
+  ${typo_body4_regular};
+  margin-bottom: 4px;
+`;
+
+const Content = styled.div`
+  ${typo_body2_medium};
+`;
+
+type Props = {
+  title: string;
+  content?: string;
+};
+
+export const TaskDescription = ({ title, content }: Props) => {
+  return (
+    <Wrapper>
+      <Title>{title}</Title>
+      <Content>{content}</Content>
+    </Wrapper>
+  );
+};
