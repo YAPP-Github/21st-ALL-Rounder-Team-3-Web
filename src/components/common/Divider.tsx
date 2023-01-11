24e9fd22bc33424d01c87eb9caee0f33a85687bc
Yejin Kim <qja086351@naver.com>
2023-01-12 01:51:34 +0900

    [feat] 디바이더 사용 방식 수정

--- src/components/common/Divider.tsx
+++ src/components/common/Divider.tsx
@@ -2,44 +2,19 @@ import styled, { css } from "styled-components";
 
 type Props = {
   height?: number;
-  marginHorizontal?: number;
   marginTop?: number;
   marginBottom?: number;
-  dividerType?: "componentDivider" | "textDivider";
 };
 
-const Divider = ({ height, marginHorizontal, marginTop, marginBottom, dividerType }: Props) => {
-  return (
-    <StyledTextDivider
-      height={height}
-      marginHorizontal={marginHorizontal}
-      marginBottom={marginBottom}
-      marginTop={marginTop}
-      dividerType={dividerType}
-    />
-  );
+const Divider = ({ height, marginTop, marginBottom }: Props) => {
+  return <BaseDivider height={height} marginBottom={marginBottom} marginTop={marginTop} />;
 };
 
-const ComponentDivider = css`
-  height: 8px;
-`;
-const TextDivider = css`
-  height: 1px;
-  margin: 10px 16px;
-`;
-
-const StyledTextDivider = styled.div`
-  background-color: #f4f4f4;
-  height: ${(props: Props) => `${props.height}px` || "1px"};
-  margin-left: ${(props: Props) => `${props.marginHorizontal}px` || "0px"};
-  margin-right: ${(props: Props) => `${props.marginHorizontal}px` || "0px"};
-  margin-top: ${(props: Props) => `${props.marginTop}px` || "0px"};
-  margin-bottom: ${(props: Props) => `${props.marginBottom}px` || "0px"};
-
-  ${props => {
-    if (props.dividerType == "componentDivider") return ComponentDivider;
-    if (props.dividerType == "textDivider") return TextDivider;
-  }}
+const BaseDivider = styled.div<{ height?: number; marginTop?: number; marginBottom?: number }>`
+  background-color: ${({ theme }) => theme.colors.gray[200]};
+  height: ${props => (props.height ? `${props.height}px` : "1px")};
+  margin-top: ${props => (props.marginTop ? `${props.marginTop}px` : "0px")};
+  margin-bottom: ${props => (props.marginBottom ? `${props.marginBottom}px` : "0px")};
 `;
 
 export default Divider;
