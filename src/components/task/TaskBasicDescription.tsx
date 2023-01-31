c2d6b0beb6b6db76b336b04e8f1b4d5d8a92cada
Yejin Kim <qja086351@naver.com>
2023-02-01 07:51:41 +0900

    [feat] 날짜 형식 정리 및 요일 변환 구현

--- src/components/task/TaskBasicDescription.tsx
+++ src/components/task/TaskBasicDescription.tsx
@@ -2,21 +2,32 @@ import styled from "styled-components";
 import Divider from "../common/Divider";
 import TaskDescription from "./TaskDescription";
 import TaskManagerProfile from "./TaskManagerProfile";
+import dayjs from "dayjs";
+
+const dayMap = ["일", "월", "화", "수", "목", "금", "토"];
 
 type Props = {
   representativeName: string;
   representativeUrl: string;
-  startDate: string;
-  dueDate: string;
+  startDate: Date;
+  dueDate: Date;
   description: string;
 };
 
+const getDayInKorean = (date: Date) => {
+  const formattedDate = dayjs(date).format("YYYY.MM.DD").toString();
+  const dayOfWeekInKorean = dayMap[Number(dayjs(date).format("d"))];
+  return formattedDate + " (" + dayOfWeekInKorean + ")";
+};
+
 const TaskBasicDescription = ({ representativeName, representativeUrl, startDate, dueDate, description }: Props) => {
+  const startDateFormatted = getDayInKorean(startDate);
+  const endDateFormatted = getDayInKorean(dueDate);
   return (
     <>
       <TaskManagerProfile name={representativeName} imageSource={representativeUrl} />
       <Divider marginBottom={10} marginTop={10} />
-      <TaskDescription title="업무 기간" content={`${startDate} ~ ${dueDate}`} />
+      <TaskDescription title="업무 기간" content={`${startDateFormatted} ~ ${endDateFormatted}`} />
       <Divider marginBottom={10} marginTop={10} />
       <TaskDescription title="업무 내용" content={description} />
     </>
