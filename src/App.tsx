60087db0619a9a2f0fb8337c087c4ee4360e37f6
woogie <dmsdmr9411@gmail.com>
2023-02-05 23:30:59 +0900

    test1

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,13 @@ import router from "./routeConfig";
 const App = () => {
   const [bottomSheet] = useRecoilState(globalBottomSheet);
 
-  httpService.setAccessToken(
-    "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIwNmU0NjY1Mi05ZjA3LTQ2OGEtOTFiZS02YmViYWNhMzg1ZWUiLCJyb2xlIjoiVVNFUiIsImV4cCI6MTY3NTY3MjQ2MH0.SpfSBRHRQn1zYmuJrNm2pCbr20YjPohlrVA0D9SB5KY",
-  );
-  httpService.get("/members");
+  // httpService.setAccessToken(
+  //   "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIwNmU0NjY1Mi05ZjA3LTQ2OGEtOTFiZS02YmViYWNhMzg1ZWUiLCJyb2xlIjoiVVNFUiIsImV4cCI6MTY3NTY3MjQ2MH0.SpfSBRHRQn1zYmuJrNm2pCbr20YjPohlrVA0D9SB5KY",
+  // );
+  // httpService.get("/members");
+  if (window.Android) {
+    window.Android.showToast("테스트 입니다.");
+  }
 
   return (
     <>
