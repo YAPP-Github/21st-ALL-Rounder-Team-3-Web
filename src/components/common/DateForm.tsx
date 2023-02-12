86b3cf4fb947639fffd9464db0de3136ef7761cf
Yejin Kim <qja086351@naver.com>
2023-02-12 02:08:03 -0500

    [refactor] DateForm 컴포넌트에서 formatDate utils 사용

--- src/components/common/DateForm.tsx
+++ src/components/common/DateForm.tsx
@@ -3,6 +3,7 @@ import { Calendar } from "react-calendar";
 import styled, { css } from "styled-components";
 import "react-calendar/dist/Calendar.css";
 import { typo_body1_medium } from "@src/styles/Typo";
+import formatDate from "@src/utils/formatDate";
 
 type Props = {
   value: Date | undefined;
@@ -10,17 +11,6 @@ type Props = {
   minDate?: Date;
   maxDate?: Date;
   onChange: (date: Date) => void;
-};
-
-const formatDate = (date: Date) => {
-  const options: Intl.DateTimeFormatOptions = {
-    year: "numeric",
-    month: "numeric",
-    weekday: "short",
-    day: "numeric",
-  };
-
-  return new Intl.DateTimeFormat("ko-KR", options).format(date);
 };
 
 const DateForm = ({ value, placeholder, minDate, maxDate, onChange }: Props) => {
