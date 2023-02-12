70cb759f813f7598eeab3e84ac51213ba18ce9a9
woogie <dmsdmr9411@gmail.com>
2023-02-12 13:37:24 +0900

    [refactor] page url 수정

--- src/constants/URLConstants.ts
+++ src/constants/URLConstants.ts
@@ -1,8 +1,6 @@
-export const TASK_PATH = "/task";
+export const TASK_PATH = "project/:projectId/task";
+export const TASK_CREATE_URL = `${TASK_PATH}/create`;
 export const TASK_DETAIL_URL = `${TASK_PATH}/:taskId`;
 export const MY_TASK_DETAIL_URL = `${TASK_PATH}/my/:taskId`;
-export const TASK_CREATE_URL = `${TASK_PATH}/create`;
-export const TASK_FEEDBACK_URL = `${TASK_PATH}/:id/feedback`;
-
-export const PROJECT_PATH = "/project";
-export const PROJECT_SETTING_URL = `${PROJECT_PATH}/setting`;
+export const TASK_EDIT_URL = `${TASK_PATH}/:taskId/edit`;
+export const TASK_FEEDBACK_URL = `${TASK_PATH}/:taskId/feedback`;
