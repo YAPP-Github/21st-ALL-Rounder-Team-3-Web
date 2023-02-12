33b452c894078259204732116f2dae2dd808c89c
woogie <dmsdmr9411@gmail.com>
2023-02-12 16:26:27 +0900

    back

---
+++ src/pages/TaskCreatePage.tsx
@@ -0,0 +1,114 @@
+import Button from "@src/components/common/Button";
+import DateForm from "@src/components/common/DateForm";
+import DropDown, { DropDownData } from "@src/components/common/DropDown";
+import Input from "@src/components/common/Input";
+import TextArea from "@src/components/common/TextArea";
+import DefaultLayout from "@src/components/layout/DefaultLayout";
+import FixedBottomButtonLayout from "@src/components/layout/FixedBottomButtonLayout";
+import useSendCreateTaskMutation from "@src/core/queries/sendCreateTaskMutation";
+import { typo_body3_regular } from "@src/styles/Typo";
+import { useMemo, useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const TEMP_DROPDOWN_DATA = [
+  { id: 1, value: "나" },
+  { id: 2, value: "은욱" },
+  { id: 3, value: "예진" },
+  { id: 4, value: "예령" },
+  { id: 5, value: "정현" },
+  { id: 6, value: "혜리" },
+  { id: 7, value: "연수" },
+  { id: 8, value: "상록" },
+  { id: 9, value: "세희" },
+  { id: 10, value: "지율" },
+];
+
+const TaskCreatePage = () => {
+  const [assignees, setAssignees] = useState<DropDownData>({ id: 1, value: "dsf" });
+  const [title, setTitle] = useState("");
+  const [memo, setMemo] = useState("");
+  const [startDate, setStartDate] = useState<Date>();
+  const [dueDate, setDueDate] = useState<Date>();
+
+  const { projectId } = useParams();
+  const { mutate } = useSendCreateTaskMutation();
+
+  const readyToCreate = !!assignees && !!title && !!startDate && !!dueDate && !!memo;
+
+  const handleCreateClick = () => {
+    if (projectId) {
+      // mutate({ projectId, participantId: assignees.id, title, memo, startDate, dueDate });
+    }
+  };
+
+  return (
+    <DefaultLayout onBack={() => {}} title="업무 생성하기">
+      <Wrapper>
+        <ListWrapper>
+          <ListTitleWrapper>
+            <ListTitle>업무 담당</ListTitle>
+          </ListTitleWrapper>
+          <DropDown data={TEMP_DROPDOWN_DATA} onChange={item => setAssignees(item)} />
+        </ListWrapper>
+        <ListWrapper>
+          <ListTitleWrapper>
+            <ListTitle>업무 제목</ListTitle>
+          </ListTitleWrapper>
+          <Input
+            value={title}
+            placeholder="업무 제목을 입력해주세요."
+            maxLength={20}
+            withError={true}
+            onChange={value => setTitle(value)}
+          />
+        </ListWrapper>
+        <ListWrapper>
+          <ListTitleWrapper>
+            <ListTitle>업무 기간</ListTitle>
+          </ListTitleWrapper>
+          <DateFormWrapper>
+            <DateForm placeholder="시작 날" value={startDate} maxDate={dueDate} onChange={date => setStartDate(date)} />
+            ~
+            <DateForm placeholder="마지막 날" value={dueDate} minDate={startDate} onChange={date => setDueDate(date)} />
+          </DateFormWrapper>
+        </ListWrapper>
+        <ListWrapper>
+          <ListTitleWrapper>
+            <ListTitle>업무 내용</ListTitle>
+          </ListTitleWrapper>
+          <TextArea value={memo} placeholder="업무와 관련한 내용을 작성해주세요." onChange={value => setMemo(value)} />
+        </ListWrapper>
+      </Wrapper>
+      <FixedBottomButtonLayout>
+        <Button type="primary" value="생성하기" onClick={() => {}} disabled={!readyToCreate} />
+      </FixedBottomButtonLayout>
+    </DefaultLayout>
+  );
+};
+
+const Wrapper = styled.div`
+  padding: 40px 16px 0;
+`;
+
+const ListWrapper = styled.div`
+  margin-bottom: 20px;
+`;
+
+const ListTitleWrapper = styled.div`
+  margin-bottom: 8px;
+`;
+
+const ListTitle = styled.p`
+  ${typo_body3_regular};
+  color: ${({ theme }) => theme.gray[600]};
+`;
+
+const DateFormWrapper = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+export default TaskCreatePage;
--- src/routeConfig.tsx
+++ src/routeConfig.tsx
@@ -1,11 +1,25 @@
 import { createBrowserRouter } from "react-router-dom";
-import { TASK_CREATE_URL, TASK_DETAIL_URL, TASK_EDIT_URL, TASK_FEEDBACK_URL } from "./constants/URLConstants";
+import {
+  MY_TASK_DETAIL_URL,
+  TASK_CREATE_URL,
+  TASK_DETAIL_URL,
+  TASK_EDIT_URL,
+  TASK_FEEDBACK_URL,
+} from "./constants/URLConstants";
+import MyTaskDetailPage from "./pages/MyTaskDetailPage";
+import OthersTaskDetailPage from "./pages/OthersTaskDetailPage";
+import TaskCreatePage from "./pages/TaskCreatePage";
+import TaskEditPage from "./pages/TaskEditPage";
 import TaskFeedbackPage from "./pages/TaskFeedbackPage";
 
 const router = createBrowserRouter([
   {
     path: TASK_DETAIL_URL,
-    element: <></>,
+    element: <OthersTaskDetailPage />,
+  },
+  {
+    path: MY_TASK_DETAIL_URL,
+    element: <MyTaskDetailPage />,
   },
   {
     path: TASK_FEEDBACK_URL,
@@ -13,11 +27,11 @@ const router = createBrowserRouter([
   },
   {
     path: TASK_CREATE_URL,
-    element: <></>,
+    element: <TaskCreatePage />,
   },
   {
     path: TASK_EDIT_URL,
-    element: <></>,
+    element: <TaskEditPage />,
   },
 ]);
 
