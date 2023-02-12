fd1740398dc8db407929aa73a4bc686c42029b6d
woogie <dmsdmr9411@gmail.com>
2023-02-12 13:34:16 +0900

    [chore] QueryClientProvider 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import App from "./App";
 import GlobalStyle from "./styles/globalstyle";
 
 const container = document.getElementById("root");
 const root = createRoot(container as Element);
+const queryClient = new QueryClient();
 
 root.render(
   <>
-    <GlobalStyle />
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <GlobalStyle />
+      <App />
+    </QueryClientProvider>
   </>,
 );
