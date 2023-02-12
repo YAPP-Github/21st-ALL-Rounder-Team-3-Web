c9f45b1cac2a596b1a9c7687995791d2d2202ee0
woogie <dmsdmr9411@gmail.com>
2023-02-13 04:02:09 +0900

    업무 생성 query 구현

---
+++ src/core/queries/useSendCreateTaskMutation.ts
@@ -0,0 +1,21 @@
+import { useMutation } from "react-query";
+import httpService from "../services/httpService";
+
+type Payload = {
+  projectId: string;
+  participantId: number;
+  title: string;
+  startDate: string;
+  dueDate: string;
+  memo: string;
+};
+
+const sendCreateTask = ({ projectId, ...payload }: Payload) => {
+  return httpService.post(`/projects/${projectId}/tasks`, { ...payload });
+};
+
+const useSendCreateTaskMutation = () => {
+  return useMutation((payload: Payload) => sendCreateTask(payload));
+};
+
+export default useSendCreateTaskMutation;
