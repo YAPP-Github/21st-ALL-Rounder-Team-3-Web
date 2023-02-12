d0dc0b513ce02285b5f13a696c8376254d2db2d2
Yejin Kim <qja086351@naver.com>
2023-02-11 22:45:10 -0500

    [chore] url 컴포넌트 분리

---
+++ src/components/task/URL.tsx
@@ -0,0 +1,35 @@
+import { typo_body4_regular, typo_h4_semibold } from "@src/styles/Typo";
+import styled from "styled-components";
+import Icons from "@src/assets/icons/index";
+
+type Props = {
+  urlLink: string;
+  urlDescription: string;
+};
+
+const URL = ({ urlLink, urlDescription }: Props) => (
+  <UrlWrapper>
+    <UrlTitle>URL</UrlTitle>
+    <UrlContent href={urlLink}>
+      <Icons.IconLink />
+      {urlDescription}
+    </UrlContent>
+  </UrlWrapper>
+);
+
+const UrlWrapper = styled.div``;
+
+const UrlTitle = styled.p`
+  ${typo_body4_regular};
+  color: ${({ theme }) => theme.gray[500]};
+  margin-bottom: 4px;
+`;
+
+const UrlContent = styled.a`
+  display: flex;
+  ${typo_h4_semibold};
+  color: ${({ theme }) => theme.black};
+  text-decoration: underline;
+`;
+
+export default URL;
