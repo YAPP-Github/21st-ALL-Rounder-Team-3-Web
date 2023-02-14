e56059d74ef184489368f443b5aaf1ba446777c6
Yejin Kim <qja086351@naver.com>
2023-02-14 03:49:47 -0500

    [chore] routeConfig 코드 재설정

--- src/routeConfig.tsx
+++ src/routeConfig.tsx
@@ -1,11 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
-import { TASK_CREATE_URL, TASK_DETAIL_URL, TASK_EDIT_URL, TASK_FEEDBACK_URL } from "./constants/URLConstants";
+import {
+  TASK_CREATE_URL,
+  TASK_DETAIL_URL,
+  MY_TASK_DETAIL_URL,
+  TASK_EDIT_URL,
+  TASK_FEEDBACK_URL,
+} from "./constants/URLConstants";
+import MyTaskDetailPage from "./pages/MyTaskDetailPage";
+import OthersTaskDetailPage from "./pages/OthersTaskDetailPage";
 import TaskFeedbackPage from "./pages/TaskFeedbackPage";
 
 const router = createBrowserRouter([
   {
     path: TASK_DETAIL_URL,
-    element: <></>,
+    element: <OthersTaskDetailPage />,
+  },
+  {
+    path: MY_TASK_DETAIL_URL,
+    element: <MyTaskDetailPage />,
   },
   {
     path: TASK_FEEDBACK_URL,
