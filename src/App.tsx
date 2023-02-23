d80f235e2de57e091e0754145e9393fd22f7c628
woogie <dmsdmr9411@gmail.com>
2023-02-23 22:57:10 +0900

    [feat] get accessTokeg

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,34 @@ import router from "./routeConfig";
 import { useEffect } from "react";
 import httpService from "./core/services/httpService";
 
+const getCookie = (cookieName: string) => {
+  const cookieData = document.cookie;
+  const findCookie = cookieData.indexOf(`${cookieName}=`) !== -1;
+
+  if (findCookie) {
+    const startIndex = cookieData.indexOf(`${cookieName}=`) + cookieName.length + 1;
+    const endIndex =
+      cookieData.indexOf(";", startIndex) !== -1 ? cookieData.indexOf(";", startIndex) : cookieData.length;
+
+    return cookieData.substring(startIndex, endIndex);
+  }
+
+  return;
+};
+
 const App = () => {
   const [bottomSheet] = useRecoilState(globalBottomSheet);
+  const accessToken = getCookie("access_token");
 
   useEffect(() => {
     // TODO: bridge 코드로 access token 가져오기
-    // httpService.setAccessToken();
-  }, []);
+    if (accessToken) {
+      console.log("ACCESS_TOKEN", accessToken);
+      httpService.setAccessToken(accessToken);
+    } else {
+      console.error("cookie does not contain access_token");
+    }
+  }, [document.cookie]);
 
   return (
     <>
